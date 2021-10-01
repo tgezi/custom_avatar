ef31d1cd96e10126b7ca2f8e913999954cd98511
tgezi <84607347+tgezi@users.noreply.github.com>
2021-10-01 17:14:43 +0800

    Main program

--- custom.py
+++ custom.py
@@ -1,20 +1,20 @@
 from PIL import Image
 
 avatar = Image.open("test.jpeg")
-nf = Image.open("national_flag.png")
+background = Image.open("national_flag.png")
 
 w, h = avatar.size
-nf = nf.resize((w, h))
+background = background.resize((w, h))
 
 for i in range(w):
     for j in range(h):
-        color = nf.getpixel((i, j))
+        color = background.getpixel((i, j))
         alpha = 255 - i // 2
         if alpha < 0:
             alpha = 0
         color = color[:-1] + (alpha,)
-        nf.putpixel((i, j), color)
+        background.putpixel((i, j), color)
 
-avatar.paste(nf, (0, 0), nf)
+avatar.paste(background, (0, 0), background)
 avatar.save("national_avatar.png")
 print("new avatar is generate!")
